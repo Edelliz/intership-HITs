847ef827384e6ae177e4c8bf51a5a901534b4020
Irina Plotnikova <80810026+Edelliz@users.noreply.github.com>
2023-03-29 23:00:27 +0700

    initial commit

---
+++ internsip-HITs/src/main/java/development/proccess/internsiphits/InternsipHiTsApplication.java
@@ -0,0 +1,13 @@
+package development.proccess.internsiphits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternsipHiTsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternsipHiTsApplication.class, args);
+    }
+
+}
